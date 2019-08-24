f3dcc34f1550bae7e1b416543a28108340c8572b
AlexYoungman95 <alexjames.youngman@gmail.com>
2019-08-24 14:31:40 +0100

    Added react-router-dom and initialisation

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      title: ""
-    };
-  }
-  render() {
-    return (
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function App() {
+  return (
+    <Router>
       <div>
-        <h1>Timekeeper</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 export default App;
 
